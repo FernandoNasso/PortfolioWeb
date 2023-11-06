a067828550fd7c7259c12d4454b3d2f72ed4f9b2
FernandoNasso <fernando.nasso@gmail.com>
2023-11-06 09:12:08 -0300

    header

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import styles from "./about.module.css"
+import { FaLocationDot } from "react-icons/fa6";
+import styles from "./about.module.css";
 
 const About = () => {
-    return (
-        <div className={styles.aboutContainer}>
-            <h1>About me</h1>
-            <p>Hola, soy Fernando! Graduado en el bootcamp de Henry. Desde Cordoba, Argentina.</p>
-            <p>en constante busqueda del desarrollo personal y profesional</p>
+  return (
+    <div className={styles.aboutContainer}>
+      <div className={styles.leftColumn}>
+        <h1>Hello, I'm <br/>Fernando!</h1>
+        <p className={styles.educationTitle}>Graduated from Henry bootcamp.</p>
+      </div>
+      <div className={styles.rightColumn}>
+        <div className={styles.locationContainer}>
+          <FaLocationDot className={styles.locationIcon} />
+          <p>From Cordoba, Argentina.</p>
         </div>
-    );
+        <p className={styles.legend}>Constantly seeking personal and professional development.</p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from "typed.js";
 import { SiRedux, SiHtml5, SiCss3, SiReact, SiExpress, SiNodedotjs, SiSequelize, SiVite } from "react-icons/si";
 import { RiJavascriptFill } from "react-icons/ri";
 import { BiLogoPostgresql, BiLogoGithub, BiLogoGit } from "react-icons/bi";
@@ -7,17 +8,47 @@ import styles from './header.module.css';
 import myPhoto from "../../assets/yo.jpeg";
 
 const Header = () => {
+  // Ref para el elemento que contendrá el efecto de escritura
+  const h1Ref = useRef(null);
+  const h2Ref = useRef(null);
+
+  useEffect(() => {
+    // Configuración de Typed.js para h1 (Fernando Nasso)
+    const optionsH1 = {
+      strings: ['Fernando Nasso'],
+      typeSpeed: 100, // Velocidad de escritura
+      showCursor: false, // Oculta el cursor de escritura
+    };
+
+    // Configuración de Typed.js para h2 (FullStack Developer)
+    const optionsH2 = {
+      strings: ['FullStack Developer'],
+      typeSpeed: 100, // Velocidad de escritura
+      showCursor: false, // Oculta el cursor de escritura
+    };
+
+    // Inicializa Typed.js en los elementos h1 y h2
+    const typedH1 = new Typed(h1Ref.current, optionsH1);
+    const typedH2 = new Typed(h2Ref.current, optionsH2);
+
+    return () => {
+      // Detiene Typed.js y limpia cuando el componente se desmonta
+      typedH1.destroy();
+      typedH2.destroy();
+    };
+  }, []);
+  
   return (
     <div className={styles.headerContainer}>
       <div className={styles.leftColumn}>
         <img src={myPhoto} alt="Fernando Nasso" className={styles.headerImage} />
         <div className={styles.textContainer1}>
-          <h1>Fernando Nasso</h1>
+          <h1 ref={h1Ref}><span></span></h1>
         </div>
       </div>
       <div className={styles.rightColumn}>
         <div className={styles.textContainer2}>
-          <h2>FullStack Developer</h2>
+        <h2 ref={h2Ref}><span></span></h2>
         </div>
         <div className={styles.iconsContainer}>
           <div className={styles.iconWrapper}>
