d78180b1b6214d50aafa3aa864dbb7dae5257334
FernandoNasso <fernando.nasso@gmail.com>
2023-11-08 14:42:13 -0300

    proyects

--- src/components/proyects/projectCard.jsx
+++ src/components/proyects/projectCard.jsx
@@ -1,71 +1,31 @@
-// import React, { useState } from "react";
-// import styles from "./projectCard.module.css";
-// import { BiLogoGithub } from "react-icons/bi";
-
-// const ProjectCard = ({ name, description, deployLink, githubLink, image }) => {
-//   const [isFlipped, setIsFlipped] = useState(false);
-
-//   const handleCardFlip = () => {
-//     setIsFlipped(!isFlipped);
-//   };
-
-//   return (
-//     <div className={isFlipped ? `${styles.card} ${styles.flipped}` : styles.card}>
-//       <div className={styles.front}>
-//         <img src={image} alt={name} />
-//         <button className={styles["info-button"]} onClick={handleCardFlip}>
-//           + Info
-//         </button>
-//       </div>
-//       <div className={styles.back}>
-//         <h2>{name}</h2>
-//         <p>{description}</p>
-//         {deployLink && (
-//           <a href={deployLink} target="_blank" rel="noopener noreferrer">
-//             Ver Proyecto
-//           </a>
-//         )}
-//         <a href={githubLink} target="_blank" rel="noopener noreferrer">
-//           <div className={styles.iconWrapper}>
-//             <BiLogoGithub className={styles.reactIcon} />
-//             <span className={styles.iconName}>GitHub</span>
-//           </div>
-//         </a>
-//         <button className={styles["info-button"]} onClick={handleCardFlip}>
-//           Return
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProjectCard;
-
 import React, { useState } from "react";
 import { BiLogoGithub } from "react-icons/bi";
-import styles from "./projectCard.module.css"; // Asegúrate de crear el archivo CSS correspondiente
+import styles from "./projectCard.module.css";
 
 const ProjectCard = ({ name, description, deployLink, githubLink, image }) => {
-  const [showDetails, setShowDetails] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
+  const toggleFlipped = () => {
+    setIsFlipped(!isFlipped);
   };
 
   return (
-    <div className={styles.projectCard}>
-      <h2>{name}</h2>
-      <img src={image} alt={name} />
-
-      <button onClick={toggleDetails}>+ Info</button>
-
-      {showDetails && (
-        <div className={styles.projectDetails}>
-          <h3>{name}</h3>
-          <p>{description}</p>
+    <div
+      className={`${styles.projectCard} ${isFlipped ? styles.flipped : ""}`}
+      onClick={toggleFlipped}
+    >
+      <div className={styles.front}>
+        <h2>{name}</h2>
+        <img src={image} alt={name} />
+        <button className={styles.infoButton}>+ info</button>
+      </div>
+      <div className={styles.back}>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <div className={styles.linksContainer}>
           {githubLink && (
             <a href={githubLink} target="_blank" rel="noopener noreferrer">
-              <BiLogoGithub className={styles.reactIcon} /> GitHub
+              <BiLogoGithub className={styles.reactIcon} /> Repository
             </a>
           )}
           {deployLink && (
@@ -73,11 +33,11 @@ const ProjectCard = ({ name, description, deployLink, githubLink, image }) => {
               Deploy
             </a>
           )}
-          <button onClick={toggleDetails}>Cerrar</button>
+          <button onClick={toggleFlipped}>Return</button>
+          </div>
         </div>
-      )}
-    </div>
-  );
-};
+      </div>
+    );
+  };
 
 export default ProjectCard;
--- src/components/proyects/proyects.jsx
+++ src/components/proyects/proyects.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import styles from "./proyects.module.css";
-import ProjectCard from "./projectCard"; // Asegúrate de que la ruta sea correcta
+import ProjectCard from "./projectCard"; 
 
 const projects = [
   {
     name: "E-commerce 'Espacio Flipante'",
     description:
-      "Trabajo realizado en grupo para culminar los estudios en Henry. Fue una experiencia de trabajo colaborativo en la creación de una aplicación completa y compleja tanto para la experiencia de usuario cliente como del administrador. Logramos una aplicación intuitiva, moderna y completa, lo que resultó en una experiencia única.",
+    "Group work, a great collaborative working experience in creating a complete and complex application with its Admin Dashboard, filters, payment gateway, database, registration, and much more.",    
     deployLink: "https://espacio-flipante-pf.vercel.app",
     githubLink: "https://github.com/FernandoNasso?tab=repositories",
-    image: require("../../assets/espacio flipante.png"), // Ruta a la imagen del proyecto
+    image: require("../../assets/espacioFlipante.PNG"), 
   },
   {
     name: "App Food",
     description:
-      "Proyecto Individual desarrollado durante mi etapa en el bootcamp de Soy Henry. Es una aplicación de una sola página (SPA) que te permite explorar una variedad de recetas, provistas por la API spoonacular, crear tus propias recetas y realizar acciones como edición y eliminación. Además, proporciona detalles exhaustivos sobre cada receta en particular, brindando una experiencia completa.",
+    "Individual Project. It's a single-page application (SPA) with full CRUD functionality, allowing you to create, edit, delete, and get details of recipes, providing a comprehensive user experience.",
     githubLink: "https://github.com/FernandoNasso/PI-Food",
-    image: require("../../assets/app food.png"),
+    image: require("../../assets/appFood.PNG"),
   },
   {
     name: "Rick & Morty App",
     description:
-      'Durante mi participación en el bootcamp de Soy Henry, desarrollé esta aplicación web. "Rick y Morty" es una plataforma que utiliza la API oficial de la serie para ofrecer información detallada sobre los personajes que aparecen en la misma. Los usuarios pueden buscar personajes, explorar sus detalles, aplicar filtros de búsqueda y guardar sus personajes favoritos para una experiencia personalizada.',
+      "This app was my first project and source of pride. It utilizes the official Rick and Morty series API to provide a complete CRUD functionality, along with filters, sorting, favorites, user registration, and more.",    
     githubLink: "https://github.com/FernandoNasso/Rick_and_Morty_Nasso",
-    image: require("../../assets/r&mLanding.png"),
+    image: require("../../assets/RickAndMorty.PNG"),
   },
 ];
 
 const Proyects = () => {
   return (
     <div className={styles.proyectsContainer}>
-      <h1>My Proyects</h1>
+      <h1>My featured projects:</h1>
       <div className={styles.cardContainer}>
         {projects.map((project, index) => (
           <ProjectCard key={index} {...project} />
